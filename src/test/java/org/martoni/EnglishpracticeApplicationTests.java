180e9782112c143134b1dcd937795b212989a7d8
Márton István <martoni779@gmail.com>
2016-03-12 15:17:11 +0100

    Kiinduló állapot

---
+++ src/test/java/org/martoni/EnglishpracticeApplicationTests.java
@@ -0,0 +1,18 @@
+package org.martoni;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EnglishpracticeApplication.class)
+@WebAppConfiguration
+public class EnglishpracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
