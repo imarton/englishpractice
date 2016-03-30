043ec2d2738585cfb3f7d14d8bcc3bc68ab53e6f
Márton István <martoni779@gmail.com>
2016-03-30 23:25:22 +0200

    prepare to run on openshift

--- src/main/java/org/martoni/EnglishPracticeApplication.java
+++ src/main/java/org/martoni/EnglishPracticeApplication.java
@@ -2,15 +2,21 @@ package org.martoni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class EnglishPracticeApplication {
+public class EnglishPracticeApplication extends SpringBootServletInitializer{
 
 //	private static final Logger log = LoggerFactory.getLogger(EnglishPracticeApplication.class);
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EnglishPracticeApplication.class);
+	}
 	
 	@RequestMapping("/")
 	public String home() {
